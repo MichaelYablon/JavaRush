10b37a5a817e88c7f2f467a1e695d46d2f5e34ce
MichaelYablon <Yablon_Michael@yahoo.com>
2017-06-02 18:08:18 +0300

    Add files via upload

---
+++ 3.JavaMultithreading/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by Miha on 27.04.2017.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public void move(){}
+    public void moveForward(){}
+}
